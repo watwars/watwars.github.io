8e948139843aad080e098704e6371f2a986dea8f
watwars <39011172+watwars@users.noreply.github.com>
2020-05-08 12:19:34 -0400

    For Kevin

--- six-grade/maths/maths.js
+++ six-grade/maths/maths.js
@@ -7,6 +7,8 @@ var arithmeticsTopics = ["<a href = 'subtraction.html'>Subtraction</a>",
 "<a href='fraction.html'>Fraction</a>",
  "<a href='percentage.html'>Percentage</a>"];
 
+ 
+
 
 function arithmetics(){
     var arithTopics = "<a href='addition.html'>Addition</a>";
@@ -14,6 +16,7 @@ function arithmetics(){
          arithTopics += '<br />' + arithmeticsTopics[i];
     }
     document.getElementById("sub-topics").innerHTML = arithTopics;
+    document.getElementById("divTopics").style.border= "1px black solid";
 }
 
 function proportion(){
@@ -30,4 +33,22 @@ function statistics(){
 
 function problemSolving(){
     document.getElementById("sub-topics").innerHTML = "Kevin est un piwa"
+}
+
+
+
+var answer = ((Math.PI * 100) - (8 * (32/ Math.sqrt(3)))) / 3 / 3;
+answer = Math.round(answer * 100) / 100;
+// alert(answer);
+function check(){
+    var useranswer = document.getElementById("hard-answer").value;
+    if(useranswer == answer){
+        alert("You got it right");
+    }else{
+        alert("You got it wrong");
+    }
+}
+
+function haha(){
+   alert("Don't give up, continue trying and you will figure out the correct answer. I cannot tell it to you.");
 }
